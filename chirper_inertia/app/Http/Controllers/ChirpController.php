9e5f4c157f4121df99888a98fdff2c1ec13f4da5
Richard Azia <richard@main-vision.com>
2022-11-27 12:03:10 +0100

    create and display chirps with vue

--- chirper_inertia/app/Http/Controllers/ChirpController.php
+++ chirper_inertia/app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChirpController extends Controller
 {
@@ -14,7 +15,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
     }
 
     /**
@@ -35,7 +38,13 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $validated = $request->validate([
+        'message' => 'required|string|max:255',
+      ]);
+
+      $request->user()->chirps()->create($validated);
+
+      return redirect(route('chirps.index'));
     }
 
     /**
--- chirper_inertia/database/migrations/2022_11_27_100438_create_chirps_table.php
+++ chirper_inertia/database/migrations/2022_11_27_100438_create_chirps_table.php
@@ -14,8 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::create('chirps', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+          $table->id();
+          $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+          $table->string('message');
+          $table->timestamps();
         });
     }
 
